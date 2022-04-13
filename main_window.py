b5bb0476087ff5ec8e778aea925a10a7dda908d7
Yu Geunhyeok <nda111@naver.com>
2022-04-13 16:25:58 +0900

    Updated: Enum, acception

--- main_window.py
+++ main_window.py
@@ -2,7 +2,7 @@ from PyQt6 import uic
 from PyQt6.QtCore import QThread, pyqtSignal
 from typing import Any
 import socket
-from protocol import resolve_request, ERequest, EResponse, RequestHandler
+from protocol import resolve_request, ERequest, EResponse, Interactor
 
 
 class MainWindow(QThread):
@@ -27,15 +27,48 @@ class MainWindow(QThread):
 
     def accept(self) -> (socket.socket, Any, ERequest):
         client, addr = self.server.accept()
-        data = client.recv(RequestHandler.BUFFER_SIZE)
+        data = client.recv(Interactor.BUFFER_SIZE)
         request, args = resolve_request(data)
 
         return client, args, request
 
     def run(self):
-        client1, addr1, role1 = self.accept()
-        client2, addr2, role2 = self.accept()
+        camera_handler, display_handler = None, None
+
+        while camera_handler is None or display_handler is None:
+            ''' 
+            Accept both camera and display handler.
+            If occupied role is proposed, send error.
+            If unknown role is proposed, send error.
+            If normal(newly role occupied), send ok.
+            '''
+            client, address, role = self.accept()
+
+            response: EResponse
+            if role == ERequest.CAMERA:
+                if camera_handler is not None:
+                    response = EResponse.ERROR
+                else:
+                    camera_handler = Interactor(client, MainWindow.handle_camera)
+                    response = EResponse.OK
+            elif role == ERequest.DISPLAY:
+                if camera_handler is not None:
+                    response = EResponse.ERROR
+                else:
+                    display_handler = Interactor(client, MainWindow.handle_display)
+                    response = EResponse.OK
+            else:
+                response = EResponse.ERROR
+            client.send(response.bytes())
 
          # TODO: networking
+
+    @staticmethod
+    def handle_camera(request: ERequest, args: bytes) -> EResponse:
+        pass
+
+    @staticmethod
+    def handle_display(request: ERequest, args: bytes) -> EResponse:
+        pass
 
     # endregion
--- protocol.py
+++ protocol.py
@@ -1,10 +1,26 @@
-from enum import Enum
+from enum import Enum, EnumMeta
 from typing import Callable
 import socket
 from PyQt6.QtCore import QThread, pyqtSignal
 
 
-class ERequest(Enum):
+class ByteEnum(Enum):
+    def int(self) -> int:
+        return int(self.value)
+
+    def bytes(self) -> bytes:
+        return bytes([self.int()])
+
+    @staticmethod
+    def from_bytes(data: bytes, enum: EnumMeta):
+        data = int(data[0])
+        for v in enum:
+            if v.int() == data:
+                return v
+        return None
+
+
+class ERequest(ByteEnum):
     CAMERA = 0x01
     DISPLAY = 0x02
 
@@ -14,58 +30,49 @@ class ERequest(Enum):
     DISPLAY_TAKE_PICTURE = 0x02 | 0x10
     DISPLAY_SHOW_PICTURE = 0x02 | 0x20
 
-    def short(self):
-        return self.value
-
     @staticmethod
-    def from_value(value):
-        for v in ERequest:
-            if v.short() == value:
-                return v
-        return None
+    def from_bytes(data: bytes, enum: EnumMeta = None):
+        return ByteEnum.from_bytes(data, ERequest)
 
 
-class EResponse(Enum):
+class EResponse(ByteEnum):
     OK = 0
     ERROR = 1
     REFUSE = 2
 
-    def short(self):
-        return self.value
-
     @staticmethod
-    def from_value(value):
-        for v in EResponse:
-            if v.short() == value:
-                return v
-        return None
+    def from_bytes(data: bytes, enum: EnumMeta = None):
+        return ByteEnum.from_bytes(data, EResponse)
 
 
 def resolve_request(data: bytes) -> (ERequest, bytes):
-    request = int.from_bytes(data[0:2], byteorder='big', signed=False)
-    request = ERequest.from_value(request)
-    args = data[2:]
+    request = ERequest.from_bytes(data[0:1])
+    args = data[1:]
     return request, args
 
 
-class RequestHandler(QThread):
+class Interactor(QThread):
     BUFFER_SIZE = 1024
     proc = pyqtSignal(ERequest, bytes, EResponse)
 
     def __init__(self,
                  client: socket.socket,
                  handler: Callable[[ERequest, bytes], EResponse]):
-        super(RequestHandler, self).__init__()
+        super(Interactor, self).__init__()
 
         self.client = client
         self.handler = handler
 
     def run(self):
         while True:
-            data = self.client.recv(RequestHandler.BUFFER_SIZE)
+            data = self.client.recv(Interactor.BUFFER_SIZE)
             request, args = resolve_request(data)
 
             response = self.handler(request, args)
             self.client.send(int.to_bytes(response.value, byteorder='big', length=2, signed=False))
 
             self.proc(request, args, response)
+
+    def request(self, request: ERequest, args: bytes):
+        data = b''.join([request.bytes(), args])
+        self.client.send(data)
