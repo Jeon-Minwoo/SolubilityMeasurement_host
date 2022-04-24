fbf7a119f967f47073399008c136fa984da7994b
Yu Geunhyeok <nda111@naver.com>
2022-04-24 15:33:00 +0900

    Updated: MainWindow
    MainWindow designed.

--- main_window.py
+++ main_window.py
@@ -1,6 +1,9 @@
 from PyQt5.QtGui import uic
 from threading import Thread
 import socket
+from PyQt5.QtCore import QSize, QRect, QMetaObject, QCoreApplication, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QGroupBox, QFileDialog
+from PyQt5.QtGui import QColor, QPalette, QPixmap, QMouseEvent, QCloseEvent
 
 from interaction.protocol import Interactor
 from interaction.bundle import Bundle
@@ -12,18 +15,127 @@ class MainWindow:
     A wrapper class for PyQt MainWindow.
     It's also a bridge between server socket and clients.
     """
+def set_widget_background_color(widget: QWidget, color: QColor):
+    palette = widget.palette()
+    palette.setColor(QPalette.Window, color)
+    widget.setPalette(palette)
+
+    widget.setAutoFillBackground(True)
+    widget.show()
+
+
+def show_image(view: QLabel, data: bytes):
+    pixmap = QPixmap()
+    if pixmap.loadFromData(data, 'JPEG'):
+        if pixmap.width() > pixmap.height():
+            pixmap = pixmap.scaledToWidth(view.width())
+        elif pixmap.width() < pixmap.height():
+            pixmap = pixmap.scaledToHeight(view.height())
+
+        view.setPixmap(pixmap)
+    else:
+        raise ValueError('Failed to load image.')
+
+
+class MainWindow(QMainWindow):
+    class ClickableLabel(QLabel):
+        double_clicked = pyqtSignal(QMouseEvent)
+
+        def mouseDoubleClickEvent(self, a0: QMouseEvent) -> None:
+            super().mouseDoubleClickEvent(a0)
+            self.double_clicked.emit(a0)
     PORT = 58431
+    instance = None
 
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super(QMainWindow, self).__init__()
 
-        form_class, window_class = uic.loadUiType("MainWindow.ui")[0]
-        self.window = window_class()
-        self.form = form_class()
-        self.form.setupUi(self.window)
+        # MainWindow
+        self.setObjectName("main_window")
+        self.resize(535, 430)
+        self.setMinimumSize(QSize(535, 430))
+        self.setMaximumSize(QSize(535, 430))
 
-        self.camera_handler = None
-        self.display_handler = None
+        self.central_widget = QWidget(self)
+        self.central_widget.setObjectName("central_widget")
+
+        # Camera Client
+        # # Front Camera
+        self.front_camera_view = QLabel(self.central_widget)
+        self.front_camera_view.setGeometry(QRect(10, 10, 256, 158))
+        self.front_camera_view.setObjectName("front_camera_view")
+
+        self.front_camera_capture_button = QPushButton(self.central_widget)
+        self.front_camera_capture_button.setGeometry(QRect(10, 170, 75, 23))
+        self.front_camera_capture_button.setObjectName("front_camera_capture_button")
+
+        self.front_camera_label = QLabel(self.central_widget)
+        self.front_camera_label.setGeometry(QRect(90, 175, 81, 16))
+        self.front_camera_label.setObjectName("front_camera_label")
+
+        # # Rear Camera
+        self.rear_camera_view = QLabel(self.central_widget)
+        self.rear_camera_view.setGeometry(QRect(270, 10, 256, 158))
+        self.rear_camera_view.setObjectName("rear_camera_view")
+
+        self.rear_camera_capture_button = QPushButton(self.central_widget)
+        self.rear_camera_capture_button.setGeometry(QRect(270, 170, 75, 23))
+        self.rear_camera_capture_button.setObjectName("rear_camera_capture_button")
+
+        self.rear_camera_label = QLabel(self.central_widget)
+        self.rear_camera_label.setGeometry(QRect(350, 175, 81, 16))
+        self.rear_camera_label.setObjectName("rear_camera_label")
+
+        # Display Client
+        # # Camera
+        self.display_camera_view = QLabel(self.central_widget)
+        self.display_camera_view.setGeometry(QRect(10, 210, 256, 158))
+        self.display_camera_view.setObjectName("display_camera_view")
+
+        self.display_camera_capture_button = QPushButton(self.central_widget)
+        self.display_camera_capture_button.setGeometry(QRect(10, 370, 75, 23))
+        self.display_camera_capture_button.setObjectName("display_camera_capture_button")
+
+        self.display_camera_label = QLabel(self.central_widget)
+        self.display_camera_label.setGeometry(QRect(90, 375, 231, 16))
+        self.display_camera_label.setObjectName("display_camera_label")
+
+        # # Displaying Image
+        self.image_path_label = MainWindow.ClickableLabel(self.central_widget)
+        self.image_path_label.setGeometry(QRect(90, 400, 231, 16))
+        self.image_path_label.setObjectName("image_path_label")
+
+        self.send_image_to_display_button = QPushButton(self.central_widget)
+        self.send_image_to_display_button.setGeometry(QRect(10, 395, 75, 23))
+        self.send_image_to_display_button.setObjectName("send_image_to_display_button")
+
+        # Client States
+        self.client_state_group_box = QGroupBox(self.central_widget)
+        self.client_state_group_box.setGeometry(QRect(280, 210, 161, 91))
+        self.client_state_group_box.setObjectName("client_state_group_box")
+
+        self.camera_state_view = QWidget(self.client_state_group_box)
+        self.camera_state_view.setGeometry(QRect(10, 30, 16, 16))
+        self.camera_state_view.setObjectName("camera_state_view")
+
+        self.camera_state_label = QLabel(self.client_state_group_box)
+        self.camera_state_label.setGeometry(QRect(30, 30, 81, 16))
+        self.camera_state_label.setObjectName("camera_state_label")
+
+        self.display_state_view = QWidget(self.client_state_group_box)
+        self.display_state_view.setGeometry(QRect(10, 60, 16, 16))
+        self.display_state_view.setObjectName("display_state_view")
+
+        self.display_state_label = QLabel(self.client_state_group_box)
+        self.display_state_label.setGeometry(QRect(30, 60, 81, 16))
+        self.display_state_label.setObjectName("display_state_label")
+
+        # Post Refactoring Process
+        self.setCentralWidget(self.central_widget)
+
+        self.init_translation()
+        self.init_components()
+        QMetaObject.connectSlotsByName(self)
         self.request_id = 0
 
         # start socket thread
@@ -59,6 +171,44 @@ class MainWindow:
                         display_handler = Interactor(client, MainWindow.handle_client_request)
                         display_handler.received_signal.connect(self.digest_response)
                         bundle.response = EResponse.OK
+        MainWindow.instance = self
+    def init_translation(self):
+        _translate = QCoreApplication.translate
+        self.setWindowTitle(_translate("main_window", "Solubility Measurement"))
+
+        self.front_camera_capture_button.setText(_translate("main_window", "Capture"))
+        self.front_camera_label.setText(_translate("main_window", "Front Camera"))
+
+        self.rear_camera_capture_button.setText(_translate("main_window", "Capture"))
+        self.rear_camera_label.setText(_translate("main_window", "Rear Camera"))
+
+        self.display_camera_capture_button.setText(_translate("main_window", "Capture"))
+        self.display_camera_label.setText(_translate("main_window", "Display Camera"))
+
+        self.send_image_to_display_button.setText(_translate("main_window", "Display"))
+        self.image_path_label.setText(_translate("main_window", "(Double click here to browse an image.)"))
+
+        self.client_state_group_box.setTitle(_translate("main_window", "Clients"))
+        self.camera_state_label.setText(_translate("main_window", "Camera"))
+        self.display_state_label.setText(_translate("main_window", "Display"))
+
+    def init_components(self):
+        set_widget_background_color(self.front_camera_view, MainWindow.Theme.GRAY.value)
+        set_widget_background_color(self.rear_camera_view, MainWindow.Theme.GRAY.value)
+        set_widget_background_color(self.display_camera_view, MainWindow.Theme.GRAY.value)
+
+        set_widget_background_color(self.camera_state_view, MainWindow.Theme.STATE_AVAILABLE.value)
+        set_widget_background_color(self.display_state_view, MainWindow.Theme.STATE_UNAVAILABLE.value)
+
+        self.front_camera_capture_button.setEnabled(False)
+        self.rear_camera_capture_button.setEnabled(False)
+        self.display_camera_capture_button.setEnabled(False)
+        self.send_image_to_display_button.setEnabled(False)
+        set_widget_background_color(self.camera_state_view,
+                                    MainWindow.Theme.STATE_UNAVAILABLE.value)
+        set_widget_background_color(self.display_state_view,
+                                    MainWindow.Theme.STATE_UNAVAILABLE.value)
+
                 else:
                     bundle.response = EResponse.ERROR
 
